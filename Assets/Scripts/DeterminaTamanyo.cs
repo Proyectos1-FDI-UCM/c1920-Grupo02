b725ad85f4b4a81bba1c59c1577052d7c17c6b22
Javier Rubio <jrubio05@ucm.es>
2020-04-14 15:34:54 +0200

    Trabajando en la batalla de la tenia

---
+++ Assets/Scripts/DeterminaTamanyo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DeterminaTamanyo : MonoBehaviour
+{
+    public float sizeMultiplier;
+
+    private Vector3 tam = new Vector3();
+
+    void Start()
+    {
+        if(sizeMultiplier > 0)
+        {
+            tam = gameObject.transform.localScale;
+
+            transform.localScale = new Vector3(sizeMultiplier * tam.x, sizeMultiplier * tam.y, 0);
+        }
+    }
+}
