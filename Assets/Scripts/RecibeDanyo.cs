4e6cad0bc5d206abaa8c33fe46417f63f4cea1a3
druiz07 <62289530+druiz07@users.noreply.github.com>
2020-04-17 17:29:48 +0200

    Creada Puerta de la Sala del Boss

--- Assets/Scripts/RecibeDanyo.cs
+++ Assets/Scripts/RecibeDanyo.cs
@@ -5,7 +5,7 @@ public class RecibeDanyo : MonoBehaviour
     public GameObject spawner;
     public int numGlobulosRojos;
     public int life;
-
+    public GameObject puerta;
     public void Dañado(int cant) // se recibe "cant" puntos de daño
     {
         life -= cant;
@@ -18,9 +18,20 @@ public class RecibeDanyo : MonoBehaviour
                     SpawnGlobulosRojos(numGlobulosRojos, gameObject.GetComponentInChildren<EnemyPatrol>().GetComponent<Transform>());
             }
             //Destruye al enemigo
+            // Habria que llamar al metodo confirma boss, que verifica si tiene el Componente Distorsion de camara
+            // Si es asi , el Objeto publico puerta, se destruye justo cuando muere la Tenia Boss
+            if (GetComponentInChildren<DistorsionCamera>()!=null) Destroy(puerta);
             Destroy(this.gameObject);
             print("Enemigo destruido");
         }
         print("enemigo es dañado por " + cant + " unidades"); /// PLACEHOLDER
     }
+
+
+
+ 
+
+
+
+
 }
