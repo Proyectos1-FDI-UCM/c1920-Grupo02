85fafb2dcb254b04b6d53abd1ffce60c8a952260
druiz07 <you@example.com>
2020-04-27 22:40:50 +0200

    Ajustado enemigo del colesterol e incluido su sprite

--- Assets/Scripts/DañoColesterol.cs
+++ Assets/Scripts/DañoColesterol.cs
@@ -1,26 +1,33 @@
 ﻿using UnityEngine;
 
-public class DanyoEnemigo2 : MonoBehaviour
+public class DañoColesterol : MonoBehaviour
 {
     public GameObject spawner;
     public int numGlobulosRojos;
     public int life;
+    public Sprite sprite;
+    SpriteRenderer actual;
+    public void Start()
+    {
+        actual = GetComponent<SpriteRenderer>();
+    }
     public void Dañado(int cant) // se recibe "cant" puntos de daño
     {
         // Variable booleana para que se bloquee uno de cada dos ataques
-        if (Random.Range(0,2) == 1)
+        if (Random.Range(0, 2) == 1)
         {
-            
+
             life -= cant;
             print("enemigo es dañado por " + cant + " unidades"); /// PLACEHOLDER
         }
         else
         {
             Debug.Log("Enemigo ha bloqueado el ataque");
-            
+
             // Se puede incluir la animacion de bloqueo del ataque 
 
         }
+        if (life <= 2) actual.sprite = sprite; //Enemigo semi dañado
         if (life <= 0)
         {
             //Spawnea globulos rojos
@@ -32,3 +39,4 @@ public class DanyoEnemigo2 : MonoBehaviour
 
     }
 }
+
