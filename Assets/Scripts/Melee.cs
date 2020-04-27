322d0927a56268d9d286d1d1cc85f9bcf6e624bc
mihern07 <mihern07@ucm.es>
2020-04-27 18:54:27 +0200

    Añadida la animación del ataque a melee y aplicadas al prefab

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -8,12 +8,15 @@ public class Melee : MonoBehaviour
 
     public GameObject ataque; // prefab a invocar para atacar
 
+    Animator animator;
+
     private void Awake()
     {
         inputActions = new PlayerInputActions();
         inputActions.PlayerControls.AtqMelee.started += ctx => meleeAtacking = true;
         inputActions.PlayerControls.AtqMelee.canceled += ctx => meleeAtacking = false;
         inputActions.PlayerControls.AtqMelee.performed += ctx => meleeAtacking = false;
+        animator = gameObject.GetComponent<Animator>();
     }
     void Update()
     {
@@ -23,6 +26,8 @@ public class Melee : MonoBehaviour
             {
                 if (meleeAtacking && GameManager.instance.GetPlayerCanAtack()) // si se desea atacar a melee y se puede:
                 {
+                    if (animator != null)
+                        animator.SetTrigger("Melee");
                     if (gameObject.transform.localScale.x == 1) // si el personaje mira a dchas.:
                     {
                         Instantiate<GameObject>(ataque, gameObject.transform.position + new Vector3(0.75f, 0, 0), Quaternion.identity, gameObject.transform); // invocación de prefab
