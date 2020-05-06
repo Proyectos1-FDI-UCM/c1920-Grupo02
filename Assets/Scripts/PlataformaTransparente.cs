b1f6b0d14e22340988ac09e9b273249449218815
javimu06 <javimu06@ucm.es>
2020-05-06 18:40:31 +0200

    Arreglado el deshabilitado de colliders con las plataformas móviles

--- Assets/Scripts/PlataformaTransparente.cs
+++ Assets/Scripts/PlataformaTransparente.cs
@@ -6,8 +6,11 @@ public class PlataformaTransparente : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collider.enabled = false;
-        Debug.Log("Me he activado");
+        if (collision.GetComponent<PlayerControllerWallJump>())
+        {
+            collider.enabled = false;
+            Debug.Log("Me he activado");
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
