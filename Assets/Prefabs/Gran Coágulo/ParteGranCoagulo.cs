0e451c2f5b5ba11ff58b8c94ff512032d9b41f85
samuebla <samuebla@ucm.es>
2020-05-17 18:38:04 +0200

    Ahora el boss del coagulo tambien parpadea cuando recibe daño

--- Assets/Prefabs/Gran Coágulo/ParteGranCoagulo.cs
+++ Assets/Prefabs/Gran Coágulo/ParteGranCoagulo.cs
@@ -4,12 +4,49 @@ public class ParteGranCoagulo : MonoBehaviour
 {
     private GranCoaguloLife vidaTotal;
     public int life;
+
+    //Variables para mostrar que recive daños
+    private float damageRecieved;
+    private SpriteRenderer sprite;
     private void Awake()
     {
         vidaTotal = gameObject.GetComponentInParent<GranCoaguloLife>();
+        sprite = gameObject.GetComponentInChildren<SpriteRenderer>();
+
+    }
+    public void Update()
+    {
+        if (sprite != null)
+        {
+            if (damageRecieved > 0)
+            {
+                damageRecieved -= Time.deltaTime;
+            }
+            else
+                sprite.color = new Color(1, 1, 1);
+
+            if (damageRecieved > 0.40)
+            {
+                sprite.color = new Color(1, 1, 1, 0.7f);
+
+            }
+            else if (damageRecieved > 0.30)
+            {
+                sprite.color = new Color(1, 1, 1);
+            }
+            else if (damageRecieved > 0.20)
+            {
+                sprite.color = new Color(1, 1, 1, 0.7f);
+            }
+            else if (damageRecieved > 0.10)
+            {
+                sprite.color = new Color(1, 1, 1);
+            }
+        }
     }
     public void Dañado(int cant) // se recibe "cant" puntos de daño
     {
+        damageRecieved = 0.5f;
         life -= cant;
         if (life <= 0)
         {
