5f002d006b43c9d16dc88590f0f9caa831a9b922
druiz07 <you@example.com>
2020-05-22 18:06:00 +0200

    Añadido efecto de bloqueo al colesterol

--- Assets/Scripts/DanyoEnemigo2.cs
+++ Assets/Scripts/DanyoEnemigo2.cs
@@ -1,17 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class DanyoEnemigo2 : MonoBehaviour
 {
     public GameObject spawner;
     public int numGlobulosRojos;
     public int life;
+    public GameObject escudo;
+    private bool activar = false;
 
     //Variables para mostrar que recive daños
     private float damageRecieved;
     private SpriteRenderer sprite;
+    private float time = 0.2f;
 
     private void Awake()
     {
+
         sprite = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
     private void Update()
@@ -48,23 +54,29 @@ public class DanyoEnemigo2 : MonoBehaviour
         }
     }
 
+
     public void Dañado(int cant) // se recibe "cant" puntos de daño
     {
         damageRecieved = 0.5f;
         // Variable booleana para que se bloquee uno de cada dos ataques
-        if (Random.Range(0,2) == 1)
+        if (Random.Range(0, 2) == 1)
         {
-            
+
             life -= cant;
             print("enemigo es dañado por " + cant + " unidades"); /// PLACEHOLDER
         }
         else
         {
+           
+
+            StartCoroutine(Activar());
             Debug.Log("Enemigo ha bloqueado el ataque");
-            
+
+
             // Se puede incluir la animacion de bloqueo del ataque 
 
         }
+
         if (life <= 0)
         {
             //Spawnea globulos rojos
@@ -73,6 +85,13 @@ public class DanyoEnemigo2 : MonoBehaviour
             Destroy(this.gameObject);
             print("Enemigo destruido");
         }
+
+    }
+    IEnumerator Activar()
+    {
+        escudo.SetActive(true);
+        yield return new WaitForSeconds(0.6f);
+        escudo.SetActive(false); 
 
     }
 }
