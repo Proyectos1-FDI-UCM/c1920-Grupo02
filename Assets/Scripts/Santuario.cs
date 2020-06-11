18c244396b33ccab32f3c63b43d19d6049ae847a
samuebla <samuebla@ucm.es>
2020-06-11 13:01:50 +0200

    Prototipo script santuario

---
+++ Assets/Scripts/Santuario.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Santuario : MonoBehaviour
+{
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si te quedas en la zona dañina...
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (timer <= 0)
+        {
+            //Y reseteas el timer
+            timer = 0.3f;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.instance.AddLife(12);
+        timer = 1;
+    }
+}
